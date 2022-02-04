61fefefc94bd794bcd1908eff7dec7c7540a5c37
mbarton40 <bart4408@fredonia.edu>
2022-02-04 13:20:42 -0500

    Found solution to problem with assigning variable names to outputs of our "newdf" function. Also, I deleted some excess code.

--- attempt020322.R
+++ attempt020322.R
@@ -27,36 +27,22 @@ totalShipment2 <- totalShipment %>%
          October = ...12, 
          November = ...13, 
          December = ...14)
-head(totalShipment2)
-
-view(totalShipment2)
 
 namesGrouped <- unique(x = totalShipment2$names)
 View(namesGrouped)
 namesGroupedUnadjusted <- totalShipment2 %>% 
   filter(substr(totalShipment2$names, 1, 1) == "U")
-head(namesGroupedUnadjusted)
-
-view(namesGroupedUnadjusted)
-nrow(namesGroupedUnadjusted)
-
 
 newdf <- function(x){
   df <- namesGroupedUnadjusted %>% 
     filter(names == x)
   return(df)
 }
-newdf("U11AVS")
-newdf("UDXDVS")
 
 unadjustedNames <- unique(namesGroupedUnadjusted$names)
-unadjustedNames
-class(unadjustedNames)
-uNames <- data.frame(unadjustedNames)
-uNames
 
-for (i in unadjustedNames) {
-  i <- newdf(i)
+for (i in unadjustedNames){
+  print(i)
+  print(newdf(i))
+  assign(x = i, value = newdf(i))
 }
-head("UDXDVS")
-
