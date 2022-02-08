4c4b438a017e92501aa73f1a0f4a4dce95bb047f
mbarton40 <bart4408@fredonia.edu>
2022-02-08 14:23:49 -0500

    Go Bills!

--- ShipmentDataTS.R
+++ ShipmentDataTS.R
@@ -61,6 +61,12 @@ for (i in unadjustedNames){
 
 plot(U11AVS, 
      col=2,
+     ylim = c(3000,12000),
      main = "U11AVS vs U11BVS Time Series",
      ylab = "Value")
 lines(U11BVS,col=3)
+legend("bottomright",                           
+       c("U11AVS","U11BVS"),
+       lty = 1,
+       col = 2:3,
+       cex = 1)
