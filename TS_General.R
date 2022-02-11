8478c4f4c7b4c95b8b79e49e0302d8b01074be33
mbarton40 <bart4408@fredonia.edu>
2022-02-11 15:15:20 -0500

    Just testing :)

--- TS_General.R
+++ TS_General.R
@@ -1,4 +1,14 @@
 source("TS_AutoReadAndSplit.R")
 
-plot(U11ATI)
-lines(U11AVS, col="red")
+plot(U11AVS, 
+     col=2,
+     ylim = c(3000,12000),
+     main = "U11AVS vs U11BVS Time Series",
+     ylab = "Value")
+lines(U11BVS,
+      col= 3)
+legend("bottomright",                           
+       c("U11AVS","U11BVS"),
+       lty = 1,
+       col = 2:3,
+       cex = 1)
