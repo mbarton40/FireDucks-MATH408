272c62c2202c2f03bbf7d6d21b45fbe75cc75943
mbarton40 <bart4408@fredonia.edu>
2022-02-26 16:23:03 -0500

    Commented through the code and made it create a few lists to contain all of the names of the time series. Also outputted several different types of data to best suit our needs in our inner "for" loop.

--- TS_AutoReadAndSplit.R
+++ TS_AutoReadAndSplit.R
@@ -1,21 +1,25 @@
+#Bringing in libraries for better functionality
 library(tidyverse)
 library(lubridate)
 library(rio)
 
+#Drawing in the data file URLs
 source("TS_DataFiles.R")
 
-count_ts = 0
+#Initializing name lists for the loop
+UnadjNameList <- c()
+UnadjNameListWide <- c()
 
 for (i in dataFiles_URL){
+  #Sets a file as a temp variable and reads it in
   FileNameToRead <- i
-  
   BaseFile <- import(FileNameToRead,
                          col_names = FALSE, 
                          col_types = c("text", "text", "numeric", "numeric", "numeric",
                                        "numeric", "numeric", "numeric",
                                        "numeric", "numeric", "numeric",
                                        "numeric", "numeric", "numeric"))
-  
+  #Renaming column names
   WideData <- BaseFile %>% 
     rename(names = ...1, 
            year = ...2,
@@ -32,28 +36,36 @@ for (i in dataFiles_URL){
            November = ...13, 
            December = ...14)
   
+  #Filters out adjusted data
   WideData_Unadjusted <- WideData %>% 
     filter(substr(WideData$names, 1, 1) == "U")
   
   #Function that splits the large wide data frame
   #into individual data frames based on name
-  
   splitTSdf <- function(x){
     TSdf <- WideData_Unadjusted %>% 
       filter(names == x)
     return(TSdf)
   }
   
+  #Appends a name list to create a chr list of all names of all ts
   TSnameList_Unadjusted <- unique(WideData_Unadjusted$names)
+  UnadjNameList <- append(UnadjNameList, TSnameList_Unadjusted)
   
-  #This for loop takes the Wide Data Frame for unadjusted Data and splits it into
+  TSnameList_Unadjusted_Wide <- paste(TSnameList_Unadjusted,"_Wide", sep = "")
+  UnadjNameListWide <- append(UnadjNameListWide, TSnameList_Unadjusted_Wide)
+  
+  #This for loop takes the Wide Data Frame for Unadjusted Data and splits it into
   #smaller data frames and then organizes them into Time Series data format.
   
   for (j in TSnameList_Unadjusted){
+    #Splits df and cuts off COVID Years...Also creates regular df object
     After_Split <- assign(x = j, value = splitTSdf(j))
     pre_Covid_AS <- After_Split %>%
       slice_head(n = 28)
     assign(x = j, value = pre_Covid_AS)
+    
+    #Formats data to get into ts and wide data objects
     temp_Long_1 <- gather(pre_Covid_AS, Month, Value, January:December, factor_key = TRUE)
     temp_Long_2 <- temp_Long_1 %>%
       mutate(Month = substring(Month,1,3)) %>%
@@ -62,15 +74,29 @@ for (i in dataFiles_URL){
       arrange(year)
     temp_Long_4 <- temp_Long_3 %>%
       select(MonYear,Value)
+    
+    #Creates time series object
     temp_Long_5 <- ts(data = temp_Long_4$Value)
     assign(x = paste(j,"_ts", sep = ""), value = temp_Long_5)
     
-    count_ts = count_ts + 1
-   
+    #Creates a wide data object
+    WideFormatDF <- as.data.frame(t(temp_Long_4))
+    colnames(WideFormatDF) <- WideFormatDF[1,]
+    WideFormatDF <- WideFormatDF[-1,]
+    assign(x = paste(j,"_Wide", sep=""), value = WideFormatDF)
+    
   }
   
-  rm(After_Split, pre_Covid_AS, BaseFile, temp_Long_1, temp_Long_2, temp_Long_3,
-     temp_Long_4, temp_Long_5, WideData, WideData_Unadjusted,
-     i, j, FileNameToRead, TSnameList_Unadjusted)
-
+  #Sorts name lists
+  UnadjNameList <- sort(UnadjNameList)
+  UnadjNameListWide <- sort(UnadjNameListWide)
+  
+  
 }
+
+#Removes unnecessary variables
+rm(After_Split, pre_Covid_AS, BaseFile, temp_Long_1, temp_Long_2, temp_Long_3,
+   temp_Long_4, temp_Long_5, WideFormatDF, WideData, WideData_Unadjusted,
+   i, j, FileNameToRead, TSnameList_Unadjusted, TSnameList_Unadjusted_Wide,
+      TS_ItS, TS_NO, TS_Shipments, TS_TI, TS_UO, TS_UOtS, dataFiles_URL)
+
