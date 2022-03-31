b38dfbfd2536c5c7171ef13f35838984e190ea16
Matt Barton <98843620+mbarton40@users.noreply.github.com>
2022-03-31 10:02:20 -0400

    small fix

--- TS_General.R
+++ TS_General.R
@@ -8,17 +8,15 @@ graphing_ts()
 idendro(h = DTW_All_Unadj_HC,
         hscale = 3.5, vscale = 2.3,
         cex = 1.25,
-        mai = c(1,1,1,3),
-        main = "Unadjusted TS DTW")
+        mai = c(1,1,1,3))
 
 #SampleMeansANOVA Cluster Function
 # our_mean_cluster(m = 30000, numSeries = 2, numOfSamples = 100)
 
 #Internal plotting (not as interactive and labels arent working)
-# plot(DTW_All_Unadj_HC,
-#      main = "Unadjusted TS DTW",
-#      cex = 0.75,
-#      hang = -1,
-#      cex = 0.5)
+plot(DTW_All_Unadj_HC,
+     main = "Unadjusted TS DTW",
+     hang = -1,
+     cex = 0.5)
 
 
